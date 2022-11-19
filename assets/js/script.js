7b14dec01efab72d37aa8d2f6ac3d2e3b70ecf1d
walter <walterrjr.86@gmail.com>
2022-11-19 08:51:40 -0300

    modulo-1-aula-13-ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,7 +53,13 @@ let nomeCompleto = `${nome} ${sobreNome}`;
 console.log(nomeCompleto); */
 
 
+let idade = 10;
 
+if (idade >= 18) {
+    console.log("Você é maior de idade.");
+}else{
+    console.log("Você é menor de idade.");
+}
 
 
 
