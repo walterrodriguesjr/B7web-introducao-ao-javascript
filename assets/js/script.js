ce05a227e5e500298b25c7812a3ec12c8b4faf10
walter <walterrjr.86@gmail.com>
2022-11-19 07:39:18 -0300

    modulo-1-aula-6-ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,27 @@
 /* alert("Hello World");
 console.log("imprimindo no log"); */
 
+
 /* let idade = 90;
 let nome = "Walter";
 console.log(nome);
 console.log(idade); */
+
+
+/* let nome = "Rodrigues";
+let idade = 36;
+let logado = true;
+let lista = ['ovo', 'cenoura', 'leite', 'chocolate'];
+let nomeCompleto = {nome: "Walter rodrigues Junior", idade: 36, cidade: "Curitiba"};
+
+console.log(nome);
+console.log(idade);
+console.log(logado);
+console.log(lista);
+console.log(nomeCompleto); */
+
+
+
 
 
 
