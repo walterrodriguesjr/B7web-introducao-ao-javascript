a02dc56f50a8ae34d980fdbad975477e54b2ac21
walter <walterrjr.86@gmail.com>
2022-11-21 09:58:04 -0300

    modulo-1-aula-19-ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -92,7 +92,13 @@ if(preco <= 45){
 } */
 
 
+/* let isMember = true;
 
+let shipping = isMember ? 2 : 10;
+
+console.log(isMember ? 'Você é membro.' : 'Você não é membro.');
+
+console.log("Frete: " + shipping); */
 
 
 
