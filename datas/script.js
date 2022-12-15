4e8a2a38ad6e012f7545ef75a9abaa905303ce7f
Walter <you@example.com>
2022-12-15 13:35:22 -0300

    modulo-8-aula-10-ok

---
+++ datas/script.js
@@ -0,0 +1,25 @@
+/* let d = new Date();
+console.log(d); */
+
+/* let d = new Date();
+console.log(d.toString()); */
+
+/* let d = new Date();
+let novoValor = d.getDay();
+console.log(novoValor); */
+
+/* let d = new Date();
+console.log(d.toUTCString()); */
+
+/* let d = new Date();
+let novoValor = d.getHours();
+console.log(novoValor); */
+
+let d = new Date();
+let novoValor = d.getMinutes();
+console.log(novoValor);
+
+
+
+
+
